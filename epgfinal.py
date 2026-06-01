a546a8baceca7082c8561ad30e49904b6a3e7a3b
Julio Cesar <104462712+JulioCesarXY@users.noreply.github.com>
2026-06-01 14:08:36 -0300

    Update US2 EPG source format from text to XML GZ
    Replaced US2 text EPG source with XML GZ version.

--- epgfinal.py
+++ epgfinal.py
@@ -8,7 +8,7 @@ EPG_SOURCES = [
     "https://epgshare01.online/epgshare01/epg_ripper_MY1.xml.gz",
     "https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/br.xml",
     "https://epgshare01.online/epgshare01/epg_ripper_NL1.xml.gz",
-    "https://epgshare01.online/epgshare01/epg_ripper_US2.txt"
+    "https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz"
 
 ]
 
