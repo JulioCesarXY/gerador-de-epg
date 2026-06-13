f5d68d369f7f7381cfbbea42d8ca88c55c2d880a
Julio Cesar <104462712+JulioCesarXY@users.noreply.github.com>
2026-06-13 13:47:53 -0300

    Update success message for empty EPG file generation

--- gerar_epg_vazio.py
+++ gerar_epg_vazio.py
@@ -53,7 +53,7 @@ def gerar_xmltv():
     with open("epg_canal_vazio.xml", "w", encoding="utf-8") as f:
         f.write(xml_bonito)
 
-    print("Sucesso: O arquivo 'epg.xml' foi gerado com base no horário do Brasil!")
+    print("Sucesso: O arquivo 'epg_canal_vazio.xml' foi gerado com base no horário do Brasil!")
 
 if __name__ == "__main__":
     gerar_xmltv()
